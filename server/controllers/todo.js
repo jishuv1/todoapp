6418eb5f6e4b261028225cc8747a8ab3a2207e2e
jishuv1 <jigyashuverma@gmail.com>
2023-06-04 12:55:10 +0545

    handling validation errors

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -10,7 +10,7 @@ export const createTodo = async (req, res) => {
     return res.json(
       jsonGenerate(
         statusCode.VALIDATION_ERROR,
-        'Todo Title should be less than 50 charaters required',
+        'Todo Title & Description should be less than 50 & 256 charaters respectively',
         error.mapped()
       )
     );
